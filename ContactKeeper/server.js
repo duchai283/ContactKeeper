60819008d40ba30556d6fa9b387b9566c19d78e9
rainbushop <duchai2803@gmail.com>
2019-07-04 18:32:44 +0700

    first initial routes and backend express

---
+++ ContactKeeper/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+///////////////
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'Welcome to the ContactKeeper API' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
